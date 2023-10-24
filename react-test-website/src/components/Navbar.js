92f0da93e8a5b4b4f2d7ac2f87d658222ae3a632
Shiv Kushwah <shivkushwah@fb.com>
2023-10-23 20:59:10 -0700

    finishing nav component

--- react-test-website/src/components/Navbar.js
+++ react-test-website/src/components/Navbar.js
@@ -5,6 +5,7 @@ function Navbar() {
     const [click, setClick] = useState(false); // Click and state handler for click variable
 
     const handleClick = () => setClick(!click); // Click handler to reverse state of click variable
+    const closeMobileMenu = () => setClick(false);
 
     return (
         <>
@@ -17,6 +18,28 @@ function Navbar() {
                         <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
                         {/* Icon from FontAwesome for hamburger settings menu, state represents which icon to display (opened vs closed)*/}
                     </div>
+                    <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                        <li className='nav-item'>
+                            <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                                Home
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/services' className='nav-links' onClick={closeMobileMenu}>
+                                Services
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/products' className='nav-links' onClick={closeMobileMenu}>
+                                Products
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/sign-up' className='nav-links-mobile' onClick={closeMobileMenu}>
+                                Sign Up
+                            </Link>
+                        </li>
+                    </ul>
                 </div>
             </nav>
         </>
